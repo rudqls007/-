c1c54c63c622fe9b5dd3102172d29fcfa84c0045
rudqls007 <rudqls007@gmail.com>
2024-03-12 21:52:06 +0900

    JPA 쿼리 메소드를 이용한 테스트 코드 작성

--- src/main/java/toy/project/repository/ItemRepository.java
+++ src/main/java/toy/project/repository/ItemRepository.java
@@ -3,5 +3,19 @@ package toy.project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import toy.project.entity.Item;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    //상품명 조회
+    List<Item> findByItemName(String itemName);
+
+    //상품명과 상품 상세 설명을 OR 조건을 이용하여 조회하는 쿼리 메소드
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetaill);
+
+    //파라미터로 넘어온 price 변수보다 값이 작은 상품 데이터를 조회하는 쿼리 메소드
+    List<Item> findByPriceLessThan(Integer price);
+
+    //OrderBy를 이용한 상품 가격 정렬
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
--- src/test/java/toy/project/repository/ItemRepositoryTest.java
+++ src/test/java/toy/project/repository/ItemRepositoryTest.java
@@ -9,14 +9,13 @@ import toy.project.constant.ItemSellStatus;
 import toy.project.entity.Item;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * @SpringBootTest
- * 통합 테스트를 위해 스프링 부트에서 제공하는 어노테이션으로 모든 Bean을 IoC 컨테이너에 등록함
- * @TestPropertySource
- * 테스트 코드 실행 시 application.properties에 설정해 둔 값보다 application-test.properties에 같은 설정이 있다면 더 높은 순위를 부여함.
+ * @SpringBootTest 통합 테스트를 위해 스프링 부트에서 제공하는 어노테이션으로 모든 Bean을 IoC 컨테이너에 등록함
+ * @TestPropertySource 테스트 코드 실행 시 application.properties에 설정해 둔 값보다 application-test.properties에 같은 설정이 있다면 더 높은 순위를 부여함.
  */
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -43,5 +42,67 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println("savedItem.toString() = " + savedItem.toString());
+    }
+
+
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(100000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNameTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemName("테스트 상품 1");
+
+        for (Item item : itemList) {
+            System.out.println("item.toString() = " + item.toString());
+
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품 상세 설명 or 테스트")
+    public void findByItemNameOrItemDetailTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNameOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+        for (Item item : itemList) {
+            System.out.println("item = " + item.toString());
+
+        }
+    }
+
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(100005);
+        for (Item item : itemList) {
+            System.out.println("item = " + item.toString());
+
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(100005);
+        for (Item item : itemList) {
+            System.out.println("item = " + item.toString());
+
+        }
     }
 }
