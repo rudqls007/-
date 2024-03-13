a1b3eb93fab2457a9edb23d7e38fc44e663d7c75
rudqls007 <rudqls007@gmail.com>
2024-03-14 01:22:25 +0900

    Spring Boot Devtools

---
+++ src/main/java/toy/project/controller/ThymeleafExController.java
@@ -0,0 +1,18 @@
+package toy.project.controller;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping("/ex01")
+    public String thymeleafExample01(Model model) {
+        model.addAttribute("data", "흥해라 쇼핑몰 !");
+        return "/thymeleafEx/thymeleafEx01";
+    }
+}
