f956ffabe87a375451b255b9efaa5398eeec9ae9
rudqls007 <rudqls007@gmail.com>
2024-03-24 23:56:06 +0900

    헤더에 로그인 정보 아이디 불러오기

--- src/main/java/toy/project/controller/MainController.java
+++ src/main/java/toy/project/controller/MainController.java
@@ -1,13 +1,19 @@
 package toy.project.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import toy.project.entity.Member;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main() {
+    public String main(Model model, @AuthenticationPrincipal Member memberInfo)throws Exception {
+
         return "index";
     }
+
 }
