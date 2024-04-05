8335e68f48ac763d2b3fb08ddee56e9b912964a4
rudqls007 <rudqls007@gmail.com>
2024-04-05 23:17:24 +0900

    OrderHist -> 로직 수정 중

---
+++ src/main/java/toy/project/dto/OrderHistDto.java
@@ -0,0 +1,33 @@
+package toy.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import toy.project.constant.OrderStatus;
+import toy.project.entity.Order;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderHistDto {
+
+    public OrderHistDto(Order order) {
+        this.orderId = order.getId();
+        this.orderDate = order.getOrderDate().format(DateTimeFormatter.ofPattern("yyyy-mm-dd HH:mm"));
+        this.orderStatus = order.getOrderStatus();
+    }
+
+    private Long orderId;
+
+    private String orderDate;
+
+    private OrderStatus orderStatus;
+
+    private List<OrderItemDto> orderItemDtoList = new ArrayList<>();
+
+    public void addOrderItemDto(OrderItemDto orderItemDto) {
+        orderItemDtoList.add(orderItemDto);
+    }
+}
